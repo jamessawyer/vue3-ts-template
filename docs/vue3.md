95170f150a94503357475556464d306d4d99b865
jamessawyer <550961784@qq.com>
2022-06-08 22:30:09 +0800

    feat: 添加unplugin-icons 自动引入element-icon

---
+++ docs/vue3.md
@@ -0,0 +1,2 @@
+1.element-plus icon自动引入
+ - [element-plus-best-practices](https://github.com/sxzz/element-plus-best-practices/blob/db2dfc983c/vite.config.ts)
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import { fileURLToPath, URL } from 'url'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
@@ -18,7 +20,13 @@ export default defineConfig({
     vueJsx(),
     AutoImport({
       imports: ['vue', 'vue-router'],
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+        ElementPlusResolver(),
+        // 自动导入图标组件
+        IconsResolver({
+          prefix: 'Icon',
+        }),
+      ],
       dts: 'src/auto-imports.d.ts',
       // 自动引入下面包含的所有模块
       dirs: [],
@@ -28,10 +36,18 @@ export default defineConfig({
     Components({
       dts: 'src/components.d.ts',
       resolvers: [
+        // Auto register icon components
+        // 自动注册图标组件
+        IconsResolver({
+          enabledCollections: ['ep'],
+        }),
         ElementPlusResolver({
           importStyle: 'sass',
         }),
       ],
+    }),
+    Icons({
+      autoInstall: true,
     }),
   ],
   resolve: {
