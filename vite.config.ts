b95228e2214bb290a375610ef247f440aaa28b33
jinx <550961784@qq.com>
2022-06-08 17:17:58 +0800

    feat: 添加auto-import 插件

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,21 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { fileURLToPath, URL } from 'url'
 import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      dts: 'src/auto-imports.d.ts',
+      // 自动引入下面包含的所有模块
+      dirs: [],
+      // 自动在VUe template中引入 默认是false
+      vueTemplate: true,
+    }),
     Components({
       dts: 'src/components.d.ts',
     }),
