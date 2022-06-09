298ab76b48238e63b53b8c4a3f589bc1a785a1a9
jinx <550961784@qq.com>
2022-06-09 14:25:35 +0800

    style(login): 登录样式调整

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -11,8 +11,16 @@
             <i-ep-avatar />
           </el-icon>
         </span>
-        <el-input placeholder="username" name="username" tabindex="1" autocomplete="on" type="text" />
+        <el-input
+          placeholder="username"
+          :input-style="{ border: 'red' }"
+          name="username"
+          tabindex="1"
+          autocomplete="on"
+          type="text"
+        />
       </el-form-item>
+
       <el-form-item prop="password">
         <span class="svg-container">
           <el-icon>
@@ -20,14 +28,97 @@
           </el-icon>
         </span>
         <el-input placeholder="password" name="password" tabindex="2" autocomplete="on" type="text" />
+        <span class="show-pwd">
+          <el-icon>
+            <i-ep-avatar />
+          </el-icon>
+        </span>
       </el-form-item>
+
       <el-button type="primary" style="width: 100%; margin-bottom: 10px">登录</el-button>
     </el-form>
   </div>
 </template>
 
-<script>
-export default {}
-</script>
+<script></script>
 
-<style></style>
+<style lang="scss" scoped>
+$bg: #2d3a4b;
+$dark_gray: #899aa4;
+$light_gray: #eee;
+$cursor: #fff;
+
+.login-container {
+  min-height: 100%;
+  width: 100%;
+  background-color: $bg;
+  overflow: hidden;
+
+  .login-form {
+    position: relative;
+    width: 520px;
+    padding: 160px 25px 0;
+    margin: 0 auto;
+    overflow: hidden;
+
+    :deep(.el-form-item) {
+      border: 1px solid rgba(255, 255, 255, 0.1);
+      background-color: rgba(0, 0, 0, 0.1);
+      border-radius: 5px;
+      color: #454545;
+    }
+    :deep(.el-input) {
+      display: inline-block;
+      height: 47px;
+      width: 85%;
+
+      .el-input__wrapper {
+        width: 100%;
+        height: 100%;
+        background: transparent;
+        // 🚨 element 使用的 box-shadow 生成的border样式
+        box-shadow: none;
+      }
+
+      input {
+        border: none;
+        background: transparent;
+        border-radius: 0;
+        padding: 12px 5px 12px 15px;
+        color: $light_gray;
+        caret-color: $cursor;
+        width: 100%;
+      }
+    }
+  }
+
+  .svg-container {
+    padding: 10px 5px 6px 15px;
+    color: $dark_gray;
+    vertical-align: middle;
+    display: inline-block;
+  }
+
+  .title-container {
+    position: relative;
+
+    .title {
+      font-size: 26px;
+      color: $light_gray;
+      margin: 0 auto 40px auto;
+      text-align: center;
+      font-weight: bold;
+    }
+  }
+
+  .show-pwd {
+    position: absolute;
+    right: 10px;
+    top: 10px;
+    font-size: 16px;
+    color: $dark_gray;
+    cursor: pointer;
+    user-select: none;
+  }
+}
+</style>
